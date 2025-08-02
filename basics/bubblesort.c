f9f8d5803b82ef727612e22ba56c3d17b14a1b64
2003GG <mohamedmoghri3301@gmail.com>
2025-08-02 19:31:47 +0100

    sorting

---
+++ basics/bubblesort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+void print(int arr[],int size){
+    for(int i=0;i<size;i++)
+       printf("%d ",arr[i]);
+    printf("\n");
+}
+void bubblesort(int arr[],int size){
+    int temp;
+    for(int i=0;i < size-1;i++){
+        for(int j=0;j < size -1 -i;j++){
+            if(arr[j] < arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    
+    int arr[]={3,2,7,8,1,4,9,6,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    
+    print(arr,size);
+    bubblesort(arr,size);
+    print(arr,size);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
